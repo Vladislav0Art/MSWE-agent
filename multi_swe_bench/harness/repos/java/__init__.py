67d77f19116a70631432e081a6bd415001f89bc5
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-04-05 01:02:40 +0200

    feat: adapt `repos.java.fasterxml` from `multi_swe_bench`
    Modifications:
    1. Create a module `..repos.java.fasterxml`
    2. Copy all py files from `multi_swe_bench`
    3. Import * from this module in `multi_swe_bench.harness.repos.java/__init__.py`

--- multi_swe_bench/harness/repos/java/__init__.py
+++ multi_swe_bench/harness/repos/java/__init__.py
@@ -8,3 +8,5 @@ from multi_swe_bench.harness.repos.java.mockito import *
 from multi_swe_bench.harness.repos.java.pmd import *
 from multi_swe_bench.harness.repos.java.ReactiveX import *
 from multi_swe_bench.harness.repos.java.spotbugs import *
+# newly added modules from multi_swe_bench
+from multi_swe_bench.harness.repos.java.fasterxml import *
