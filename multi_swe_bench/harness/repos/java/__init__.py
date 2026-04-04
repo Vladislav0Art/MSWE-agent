e076c2e7e22060427f2beea000714a3ff88f0b72
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-04-05 01:13:58 +0200

    feat: adapt `repos.java.googlecontainertools` from `multi_swe_bench`

--- multi_swe_bench/harness/repos/java/__init__.py
+++ multi_swe_bench/harness/repos/java/__init__.py
@@ -14,3 +14,4 @@ from multi_swe_bench.harness.repos.java.Graylog2 import *
 from multi_swe_bench.harness.repos.java.OpenRefine import *
 from multi_swe_bench.harness.repos.java.apache import *
 from multi_swe_bench.harness.repos.java.google import *
+from multi_swe_bench.harness.repos.java.googlecontainertools import *
