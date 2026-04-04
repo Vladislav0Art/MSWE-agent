ad3cbbcad299bc6cb25596343890ab7a29b17589
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-04-05 01:14:50 +0200

    feat: adapt `repos.java.provectus` from `multi_swe_bench`

--- multi_swe_bench/harness/repos/java/__init__.py
+++ multi_swe_bench/harness/repos/java/__init__.py
@@ -16,3 +16,4 @@ from multi_swe_bench.harness.repos.java.apache import *
 from multi_swe_bench.harness.repos.java.google import *
 from multi_swe_bench.harness.repos.java.googlecontainertools import *
 from multi_swe_bench.harness.repos.java.micronautprojects import *
+from multi_swe_bench.harness.repos.java.provectus import *
