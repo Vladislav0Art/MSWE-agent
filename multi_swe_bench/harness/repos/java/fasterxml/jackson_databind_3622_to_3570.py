9f7a4d2e4d8593c52cb168f73145025de268b5e3
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-04-16 02:36:19 +0200

    feat: metamorphic for `jackson_databind`

--- multi_swe_bench/harness/repos/java/fasterxml/jackson_databind_3622_to_3570.py
+++ multi_swe_bench/harness/repos/java/fasterxml/jackson_databind_3622_to_3570.py
@@ -5,6 +5,7 @@ from typing import Optional, Union
 from multi_swe_bench.harness.image import Config, File, Image
 from multi_swe_bench.harness.instance import Instance, TestResult
 from multi_swe_bench.harness.pull_request import PullRequest
+from multi_swe_bench.harness.metamorphic import Metamorphic
 
 
 class ImageDefault(Image):
@@ -125,11 +126,18 @@ RUN git clone https://github.com/fasterxml/jackson-databind.git /home/jackson-da
 WORKDIR /home/jackson-databind
 RUN git reset --hard
 RUN git checkout {pr.base.sha}
+
+# apply metamorphic patch (if present)
+{metamorphic_patch_cmd}
 """
         dockerfile_content += f"""
-{copy_commands}
+{{copy_commands}}
 """
-        return dockerfile_content.format(pr=self.pr)
+        return dockerfile_content.format(
+            pr=self.pr,
+            metamorphic_patch_cmd=Metamorphic.apply_metamorphic_patch_cmd(pr=self.pr),
+            copy_commands=copy_commands,
+        )
 
 
 @Instance.register("fasterxml", "jackson_databind_3622_to_3570")
